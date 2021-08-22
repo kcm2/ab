e8be4c366364ce070df44468bd237eee05fcadd9
KCM2 <kcm2@chiyo-chan.me>
2021-08-22 17:23:14 +0100

    more robots testing

--- conf.py
+++ conf.py
@@ -55,12 +55,14 @@ html_show_sourcelink = True
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# Add any extra paths that contain custom files (such as robots.txt or
+# .htaccess) here, relative to this directory. These files are copied
+# directly to the root of the documentation.
+#
+html_extra_path = ['./robots.txt']
+
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 #html_css_files = [
 #    '../ccss/custom.css',
 #]
-
-html_extra_path = [
-    'robots.txt'
-]
